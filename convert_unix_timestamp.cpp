caf396cb49d1fc168b7350a197ad66f76c253a6b
andreir312 <andreirar24@gmail.com>
2017-05-16 11:35:51 +0300

    Exemplu de cum putem converti un unix timestamp.

---
+++ convert_unix_timestamp.cpp
@@ -0,0 +1,19 @@
+#include <ctime>
+#include <string>
+#include <iostream>
+
+int main ()
+{
+    int rawtime = 725068800;
+    time_t aux = rawtime;
+    struct tm *timeinfo;
+    char buffer[5];
+
+    timeinfo = gmtime (&aux);
+
+    strftime(buffer, 5, "%Y", timeinfo);
+    std::string s(buffer);
+    std::cout << s << std::endl;
+
+    return 0;
+}
