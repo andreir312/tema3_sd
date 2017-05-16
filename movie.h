1cb610f356d0694cfb15b3764a788bbc15baf2fa
andreir312 <andreirar24@gmail.com>
2017-05-16 12:21:13 +0300

    Rating management modifications.

--- movie.h
+++ movie.h
@@ -20,7 +20,53 @@ struct Movie
 
     double rating;
 
-    int votes;
+    double votes;
+
+    double sum;
+
+    Movie(std::string movie_name, std::string movie_id, int timestamp,
+          std::vector<std::string> categories, std::string director_name,
+          std::vector<std::string> actor_ids)
+    {
+        this->name = movie_name;
+
+        this->id = movie_id;
+
+        this->timestamp = timestamp;
+
+        this->categories = categories;
+
+        this->director_name = director_name;
+
+        this->actors_ids = actor_ids;
+    }
+
+    add_rating(int new_rating)
+    {
+        this->votes += 1;
+
+        this->sum += (double)new_rating;
+
+        this->rating = this->sum / this->votes;
+    }
+
+    update_rating(int new_rating, int old_rating)
+    {
+        this->sum -= (double)old_rating;
+
+        this->sum += (double)new_rating;
+
+        this->rating = this->sum / this->votes;
+    }
+
+    remove_rating(int old_rating)
+    {
+        this->votes -= 1;
+
+        this->sum -= (double)old_rating;
+
+        this->rating = this->sum / this->votes;
+    }
 };
 
 #endif  // MOVIE__H__
--- user.h
+++ user.h
@@ -2,12 +2,32 @@
 #define USER__H__
 
 # include <string>
+# include <unordered_map>
 
 struct User
 {
     std::string id;
 
     std::string name;
+
+    std::unordered_map<std::string, double> movies;
+
+    User(std::string user_id, std::string name)
+    {
+        this->id = user_id;
+
+        this->name = name;
+    }
+
+    sync_rating(std::string movie_id, int rating)
+    {
+        movies[movie_id] = (double)rating; // works with both add and update
+    }
+
+    remove_rating(std::string movie_id)
+    {
+        movies.erase(movie_id);
+    }
 };
 
 #endif  // USER__H__
